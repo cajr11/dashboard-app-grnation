253b1aca30b52f58db7ca357dee2f4c38d7b0740
cajr11 <caondo@yahoo.co.uk>
2021-07-15 22:14:09 -0500

    added helper for getting results page

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -15,3 +15,13 @@ export const getResultsPerPageProducts = function (page, currState) {
   const end = page * currState.loaded.resultsPerPage;
   return currState.info.products.sort((a, b) => a.id - b.id).slice(start, end);
 };
+
+export const getResultsPerPageOrders = function (page, currState) {
+  currState.loaded.page = page;
+
+  const start = (page - 1) * currState.loaded.resultsPerPage;
+  const end = page * currState.loaded.resultsPerPage;
+  return currState.fullData
+    .sort((a, b) => a.userID - b.userID)
+    .slice(start, end);
+};
